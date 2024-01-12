5496d3594d6e5549770645282a14054d1379905f
Samuel Gunter <samathingamajig@gmail.com>
2024-01-12 05:44:49 -0600

    comparison operators
    book section 3.4

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -22,6 +22,7 @@ func New() *Compiler {
 
 func (c *Compiler) Compile(node ast.Node) error {
 	switch node := node.(type) {
+	// Statements
 	case *ast.Program:
 		for _, s := range node.Statements {
 			err := c.Compile(s)
@@ -36,7 +37,22 @@ func (c *Compiler) Compile(node ast.Node) error {
 		}
 		c.emit(code.OpPop)
 
+	// Expressions
 	case *ast.InfixExpression:
+		if node.Operator == "<" {
+			err := c.Compile(node.Right)
+			if err != nil {
+				return err
+			}
+			err = c.Compile(node.Left)
+			if err != nil {
+				return err
+			}
+
+			c.emit(code.OpGreaterThan)
+			return nil
+		}
+
 		err := c.Compile(node.Left)
 		if err != nil {
 			return err
@@ -55,6 +71,12 @@ func (c *Compiler) Compile(node ast.Node) error {
 			c.emit(code.OpMul)
 		case "/":
 			c.emit(code.OpDiv)
+		case ">":
+			c.emit(code.OpGreaterThan)
+		case "==":
+			c.emit(code.OpEqual)
+		case "!=":
+			c.emit(code.OpNotEqual)
 		default:
 			return fmt.Errorf("unknown operator: %s", node.Operator)
 		}
