4fc02b44c9d768d7537e8f6505bf6a80c8eadf2b
Samuel Gunter <samathingamajig@gmail.com>
2024-01-18 18:50:06 -0600

    indices are back, baby!
    book section 6.4

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -108,6 +108,19 @@ func (c *Compiler) Compile(node ast.Node) error {
 
 		c.emit(code.OpArray, len(node.Elements))
 
+	case *ast.IndexExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Index)
+		if err != nil {
+			return err
+		}
+
+		c.emit(code.OpIndex)
+
 	case *ast.IfExpression:
 		err := c.Compile(node.Condition)
 		if err != nil {
--- vm/vm.go
+++ vm/vm.go
@@ -165,6 +165,15 @@ func (vm *VM) Run() error {
 			if err != nil {
 				return err
 			}
+
+		case code.OpIndex:
+			index := vm.pop()
+			left := vm.pop()
+
+			err := vm.executeIndexExpression(left, index)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
@@ -360,4 +369,42 @@ func (vm *VM) buildHash(startIndex, endIndex int) (object.Object, error) {
 	}
 
 	return &object.Hash{Pairs: hashedPairs}, nil
+}
+
+func (vm *VM) executeIndexExpression(left, index object.Object) error {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return vm.executeArrayIndexExpression(left, index)
+	case left.Type() == object.HASH_OBJ:
+		return vm.executeHashIndexExpression(left, index)
+	default:
+		return fmt.Errorf("index operator not supported: %s[%s]", left.Type(), index.Type())
+	}
+}
+
+func (vm *VM) executeArrayIndexExpression(array, index object.Object) error {
+	arrayObject := array.(*object.Array)
+	i := index.(*object.Integer).Value
+	max := int64(len(arrayObject.Elements) - 1)
+
+	if i < 0 || i > max {
+		return vm.push(NULL)
+	}
+
+	return vm.push(arrayObject.Elements[i])
+}
+
+func (vm *VM) executeHashIndexExpression(hash, index object.Object) error {
+	hashObject := hash.(*object.Hash)
+	key, ok := index.(object.Hashable)
+	if !ok {
+		return fmt.Errorf("unusable as hash key: %s", index.Type())
+	}
+
+	pair, ok := hashObject.Pairs[key.HashKey()]
+	if !ok {
+		return vm.push(NULL)
+	}
+
+	return vm.push(pair.Value)
 }
